9db4d7e9bf56ede69e7b72e213ee43769cb0d917
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2023-01-15 19:24:24 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiThread
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Thread thread1;
+        private Thread thread2;
+        private Numbers numbers;
+        public MainWindow()
+        {
+            InitializeComponent();
+            numbers= new Numbers();
+            this.DataContext= numbers;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            thread1 = new Thread(numbers.GenerateFibanacci);
+            thread2=new Thread(numbers.GeneratePrimary);
+        }
+
+        private void bStartFT_Click(object sender, RoutedEventArgs e)
+        {
+            this.thread1.Start();
+        }
+
+        //private void bStopFT_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.thread1.Abort();
+        //}
+
+        //private void bRefreshFT_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.thread1.Interrupt();
+        //}
+
+        //private void bPauseFT_Click(object sender, RoutedEventArgs e)
+        //{
+        //    thread1.Suspend();
+        //}
+
+        //private void bResumeFT_Click(object sender, RoutedEventArgs e)
+        //{
+        //    thread1.Resume();
+        //}
+
+        private void bStartST_Click(object sender, RoutedEventArgs e)
+        {
+            thread2.Start();
+        }
+
+        //private void bStopST_Click(object sender, RoutedEventArgs e)
+        //{
+        //    thread2?.Abort();
+        //}
+
+        //private void bPauseST_Click(object sender, RoutedEventArgs e)
+        //{
+        //    thread2.Suspend();
+        //}
+
+        //private void bResumeST_Click(object sender, RoutedEventArgs e)
+        //{
+        //    thread2.Resume();
+        //}
+
+        //private void bRefreshST_Click(object sender, RoutedEventArgs e)
+        //{
+        //    thread2.Interrupt();
+        //}
+    }
+}
---
+++ Numbers.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Threading;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+
+namespace MultiThread
+{
+    public  class Numbers:INotifyPropertyChanged
+    {
+        private string _valueF;
+        private int _minF;
+        private int _topF;
+        private string _valueP;
+        private int _minP;
+        private int _topP;
+        public string ValueSth
+        {
+            get
+            {
+                return _valueP;
+            }
+            set
+            {
+                _valueP = value;
+                NotifyPropertyChanged(nameof(ValueSth));
+            }
+        }
+
+        public int MinSth
+        {
+            get
+            {
+                return _minP;
+            }
+            set
+            {
+                _minP = value;
+                NotifyPropertyChanged(nameof(MinSth));
+            }
+        }
+        public int TopSth
+        {
+            get
+            {
+                return _topP;
+            }
+            set
+            {
+                _topP = value;
+                NotifyPropertyChanged(nameof(TopSth));
+            }
+        }
+        public string ValueFTh { 
+            get { 
+                return _valueF;
+            }
+           
+            set { 
+                _valueF = value;
+                NotifyPropertyChanged(nameof(ValueFTh));
+            } }
+        public int BottomFTh
+        {
+            get
+            {
+                return _minF;
+            }
+            set
+            {
+                _minF = value;
+                NotifyPropertyChanged(nameof(BottomFTh));
+            }
+        }
+        public int TopFTh
+        {
+            get { return _topF; }
+            set
+            {
+                _topF = value;
+                NotifyPropertyChanged(nameof(TopFTh));
+            }
+        }
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public  void GenerateFibanacci(object ?data)
+        {
+            int a = 0, b = this.BottomFTh, c = 0;
+            while ((a + b )< this.TopFTh)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Thread.Sleep(1000);
+                ValueFTh = c.ToString();
+            }
+        }
+        public void GeneratePrimary(object? data)
+        {
+            for(int i = this.MinSth;i<this.TopSth;i++) {
+                bool IsPrime = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if(i%j== 0)
+                    {
+                        IsPrime=false;
+                        break;
+                    }
+                }
+                if (IsPrime)
+                {
+                    this.ValueSth=i.ToString();
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
